4d23f78e94fb4da067eda5d7e61b2a96b00d1587
pawan <noreply.1824@gmail.com>
2021-08-12 11:00:48 +0530

    added custom app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,36 @@
-import { ProvideAuth } from "../lib/auth";
+import { AuthProvider } from "@/lib/auth";
+import { Global, css } from "@emotion/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import theme from "@/styles/theme";
+// TODO: Understand / remove this
+const GlobalStyle = ({ children }) => (
+  <>
+    <CSSReset />
+    <Global
+      styles={css`
+        html {
+          min-width: 360px;
+          scroll-behavior: smooth;
+        }
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+          background: white;
+        }
+      `}
+    />
+    {children}
+  </>
+);
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
